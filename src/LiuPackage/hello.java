cafa75e366eb122067f55103ceff62f7353ff559
handsome <50476352+lzwoaini@users.noreply.github.com>
2021-03-18 15:25:23 +0800

    Update hello.java

--- src/LiuPackage/hello.java
+++ src/LiuPackage/hello.java
@@ -4,6 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("hello world!chen");
-        System.out.println("hello world! liu");
+        System.out.println("hello world! liu
+        System.out.println("hello world! unknow");
     }
 }
