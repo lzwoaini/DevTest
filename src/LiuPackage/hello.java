75fb5cef434b9f8f30a62dfca40e93e8902a2aa6
LittleLion <3439163714@qq.com>
2021-03-18 16:09:30 +0800

    add a sout 2 again三生三世2

--- src/LiuPackage/hello.java
+++ src/LiuPackage/hello.java
@@ -9,5 +9,6 @@ public class hello {
         System.out.println("hello world! liu2");
         System.out.println("hello world! 陈明瀚");
         System.out.println("hello world! LLLLLiu");
+        System.out.println("hello world! LLLLLiu2");
     }
 }
