754dff4b39107c714f4d1b75c9d210e7d7ed9808
陈明瀚 <58127395+Bhind1003@users.noreply.github.com>
2021-03-18 16:43:45 +0800

    add a 陈明瀚 three times

--- src/LiuPackage/hello.java
+++ src/LiuPackage/hello.java
@@ -8,5 +8,11 @@ public class hello {
         System.out.println("hello world! unknow");
         System.out.println("hello world! liu2");
         System.out.println("hello world! 陈明瀚");
+        System.out.println("hello world! 陈明瀚2");
+        System.out.println("hello world! LLLLLiu");
+        System.out.println("hello world! LLLLLiu2");
+        System.out.println("hello world! LLLLLiu3");
+        System.out.println("hello world! 陈明瀚3");
+
     }
 }
